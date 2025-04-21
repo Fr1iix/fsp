9b400fbc30755e6a21bddaa7a899d597be14e3da
misha <misha_123@example.com>
2025-04-21 18:31:17 +0300

    Registration and start files

--- server/src/models/models.js
+++ server/src/models/models.js
@@ -4,19 +4,18 @@ const {DataTypes} = require('sequelize')
 const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     email: {type: DataTypes.STRING, unique: true},
-    password: {type: DataTypes.STRING, allowNull: false},
-    phone: {type: DataTypes.CHAR},
-    role: {type: DataTypes.STRING, defaultValue: 'user'}
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: 'user'},
 })
 
 const UserInfo = sequelize.define('user_info', {
     firstName: {type: DataTypes.STRING},
     lastName: {type: DataTypes.STRING},
     middleName: {type: DataTypes.STRING},
-    birthday: {type: DataTypes.STRING},
+    birthday: {type: DataTypes.DATE},
     gender: {type: DataTypes.STRING},
     address: {type: DataTypes.STRING},
-    age: {type: DataTypes.INTEGER},
+    phone: {type: DataTypes.STRING},
 })
 
 User.hasOne(UserInfo, {
