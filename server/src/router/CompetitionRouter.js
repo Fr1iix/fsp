25bab5a6d7cd8441668628da0551ba135df7a9c1
JAVE232323 <a.tyulmenev@yandex.ru>
2025-04-23 01:39:16 +0300

    добавсление к роутеру post запрос на создание соревнования

--- server/src/router/CompetitionRouter.js
+++ server/src/router/CompetitionRouter.js
@@ -5,6 +5,7 @@ const CompetitionController = require('../controller/CompetitionController')
 router.delete('/deleteCompetition/:id', CompetitionController.deleteComp)
 router.put("/updateCompetition/:id", CompetitionController.updateOne)
 router.get("/Competition/:id", CompetitionController.getOneCompetition)
-router.get("/getCompetition", CompetitionController.getAll)
+router.get("/Competition", CompetitionController.getAll)
+router.post("/Competition", CompetitionController.create)
 
 module.exports = router
