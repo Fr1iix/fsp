9a96d08cfaf6646fe827ea42aea8ca0ac1520d09
misha <misha_123@example.com>
2025-04-23 15:49:09 +0300

    Рабочий вроде

--- server/src/router/ApplicationRouter.js
+++ server/src/router/ApplicationRouter.js
@@ -2,10 +2,7 @@ const Router = require('express')
 const router = new Router()
 const ApplicationController = require('../controller/ApplicationController')
 const authMiddleware = require('../middleware/authmiddleware')
-const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
 
-// Получение всех заявок (только для администраторов и ФСП)
-router.get('/', authMiddleware, checkRoleMiddleware(['admin', 'fsp']), ApplicationController.getAll)
 
 // Получение заявок конкретного пользователя
 router.get('/user/:userId', authMiddleware, ApplicationController.getByUser)
@@ -15,9 +12,6 @@ router.get('/:id', authMiddleware, ApplicationController.getOne)
 
 // Создание новой заявки
 router.post('/', authMiddleware, ApplicationController.create)
-
-// Обновление статуса заявки (только для ФСП)
-router.patch('/:id/status', authMiddleware, checkRoleMiddleware(['fsp']), ApplicationController.updateStatus)
 
 // Полное обновление заявки
 router.put('/:id', authMiddleware, ApplicationController.updateOne)
